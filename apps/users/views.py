0198f85c0df2b399987f9526b2ba6cd499fb5aa2
Saidulloh <ssavutikhunov@gmail.com>
2024-04-11 16:26:15 +0600

    add get request for showing my restaurants and my reviews

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from apps.users.serializers import UserSerializer
+from apps.restaurants.models import Restaurant
+from apps.restaurants.serializers import RestaurantSerializer
+from apps.reviews.models import Review
+from apps.reviews.serializers import ReviewSerialzier
 from utils.Permissions import IsOwner
 
 
@@ -21,4 +25,20 @@ class UserApiViewSet(ModelViewSet):
     )
     def current_user(self, request):
         serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=False, permission_classes=[IsAuthenticated], methods=['get']
+    )
+    def my_restaurants(self, request):
+        restaurants = Restaurant.objects.filter(owner=request.user)
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=False, permission_classes=[IsAuthenticated], methods=['get']
+    )
+    def my_reviews(self, request):
+        reviews = Review.objects.filter(review_owner=request.user)
+        serializer = ReviewSerialzier(reviews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
